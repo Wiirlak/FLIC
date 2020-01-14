31c529ccb939c4ef982c1d4f95e19fa374bb399b
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-01-14 20:40:20 +0100

    Starting - Main Activity & Fragments : Location

---
+++ app/src/main/java/fr/esgi/flic/Location.java
@@ -0,0 +1,36 @@
+package fr.esgi.flic;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Location extends Fragment {
+
+    private LocationViewModel mViewModel;
+
+    public static Location newInstance() {
+        return new Location();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.location_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(LocationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
