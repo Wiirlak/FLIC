9b9dc6b9882619667094188a3d274932de979d42
Zineddine <zinedddine97@gmail.com>
2020-01-25 22:41:34 +0100

    add Widget tests on database

--- app/src/main/java/fr/esgi/flic/WidgetProvider.java
+++ app/src/main/java/fr/esgi/flic/WidgetProvider.java
@@ -1,6 +1,98 @@
 package fr.esgi.flic;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
 
 public class WidgetProvider extends AppWidgetProvider {
+
+    private static final String TAG = "AppWidgetProvider";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    public void onEnabled(Context context) {
+        Log.d(TAG, "onEnabled");
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+        SharedPreferences prefs = context.getApplicationContext().getSharedPreferences("data", 0);
+
+        String coupled_id = prefs.getString("coupled_id", "senyuhG15nVVusKgX9ul");
+
+        DocumentReference docRef = db.collection("user").document(coupled_id);
+        final CollectionReference notifications = db.collection("notifications");
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        Query query = db.collection("notifications")
+                                .whereEqualTo("user_id", document.getData())
+                                .orderBy("Time", Query.Direction.DESCENDING)
+                                .limit(1);
+                        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    QuerySnapshot document = task.getResult();
+                                    if (!document.isEmpty()) {
+                                        Log.d(TAG, "QuerySnapshot data: " + document.getDocuments());
+                                    } else {
+                                        Log.d(TAG, "No such document");
+                                    }
+                                } else {
+                                    Log.d(TAG, "get failed (querysnapshot) with ", task.getException());
+                                }
+                            }
+                        });
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            Log.d(TAG, "onUpdate");
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_flic);
+            views.setOnClickPendingIntent(R.id.button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Log.d(TAG, "onDeleted");
+    }
 }
