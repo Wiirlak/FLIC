12bf0f3dd23c87aa36f1347a7344fef410419474
Alexis <vizuar.gamer@gmail.com>
2020-01-26 16:18:15 +0100

    LinkActivity - back function

--- app/src/main/java/fr/esgi/flic/LinkActivity.java
+++ app/src/main/java/fr/esgi/flic/LinkActivity.java
@@ -2,8 +2,13 @@ package fr.esgi.flic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class LinkActivity extends AppCompatActivity {
 
@@ -12,5 +17,12 @@ public class LinkActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_link);
+    }
+
+    public void onClickCopyLink(View view) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+//        ClipData clip = ClipData.newPlainText("id", (TextView) findViewById(R.id.personnalID)());
+        ClipData clip = ClipData.newPlainText("id", "Moncode");
+        clipboard.setPrimaryClip(clip);
     }
 }
