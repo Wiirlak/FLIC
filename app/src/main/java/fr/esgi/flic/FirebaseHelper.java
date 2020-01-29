380a9a9ac1f1a5a41a9bd16c1e690bef823ca88c
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-01-29 23:01:46 +0100

    FirebaseHelper patched | adding db on login page

--- app/src/main/java/fr/esgi/flic/FirebaseHelper.java
+++ app/src/main/java/fr/esgi/flic/FirebaseHelper.java
@@ -1,9 +1,12 @@
 package fr.esgi.flic;
 
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class FirebaseHelper {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -15,13 +18,37 @@ public class FirebaseHelper {
     }
 
     public Object post(String collection, String id, Map<String, Object> data) {
-        Task result = db.collection(collection).document(id).set(data);
-        return result.getResult();
+        Task result;
+        try {
+            result = db.collection(collection).document(id).set(data);
+            Tasks.await(result);
+            return result.getResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public Object post(String collection, String id, String field, Object value) {
+        Map<String, Object> data = new HashMap<>();
+        data.put(field, value);
+        Task result;
+        try {
+            result = db.collection(collection).document(id).set(data);
+            Tasks.await(result);
+            return result.getResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public Object delete(String collection, String id, Map<String, Object> data) {
         Task result = db.collection(collection).document(id).delete();
         return result.getResult();
+    }
+
+    public boolean exist(String id){
+        return false;
     }
 
 }
--- app/src/main/java/fr/esgi/flic/LinkActivity.java
+++ app/src/main/java/fr/esgi/flic/LinkActivity.java
@@ -3,6 +3,7 @@ package fr.esgi.flic;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -12,7 +13,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class LinkActivity extends AppCompatActivity {
+    FirebaseHelper db;
     ClipboardManager clipboard;
     CharSequence id_user;
     CharSequence id_partner; // can be null
@@ -23,10 +27,13 @@ public class LinkActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_link);
         clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        db = new FirebaseHelper();
 
         TextView idUser = findViewById(R.id.personnalID);
-        id_user = idUser.getText();
-        waitingMessage();
+        //id_user = idUser.getText();
+        id_user = generateId();
+        idUser.setText(id_user);
+        waitingMessage(false);
     }
 
     @Override
@@ -42,27 +49,58 @@ public class LinkActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), "Ton id a été mis dans le presse-papier !", Toast.LENGTH_SHORT).show();
     }
 
+    public void setPartner(View view) {
+        TextView partner = findViewById(R.id.companionID);
+        id_partner  = partner.getText();
+        db.post("user", id_user.toString(), "partner_id", "user/" + id_partner.toString());
+
+        Toast.makeText(getApplicationContext(), "id envoyé", Toast.LENGTH_SHORT).show();
+        waitingMessage(true);
+    }
+
     public void applyCopiedLinkOnStart() {
         ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
         id_partner  = item.getText();
         if (id_partner != null) {
-            if(!id_partner.toString().equals(id_user.toString())) {
-                EditText idPartner = findViewById(R.id.companionID);
-                idPartner.setText(id_partner);
-                idPartner.setSelection(id_partner.length());
+            if(id_partner.length() == 9) {
+                if (!id_partner.toString().equals(id_user.toString())) {
+                    EditText idPartner = findViewById(R.id.companionID);
+                    idPartner.setText(id_partner);
+                    idPartner.setSelection(id_partner.length());
+                }
             }
         }
 
     }
 
-    public void waitingMessage() {
+    public void waitingMessage(boolean v) {
+        TextView partner = findViewById(R.id.companionID);
         TextView waitMessage = findViewById(R.id.waitText);
-        if(false){
+        if(v){
             waitMessage.setVisibility(TextView.VISIBLE);
+            partner.setFocusable(false);
+            partner.setBackgroundColor(Color.argb(200,122,122,122));
         }else{
             waitMessage.setVisibility(TextView.INVISIBLE);
         }
 
+    }
+
+    public String generateId(){
+        int length = 6;
+        char[] base62chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i=0; i<length; i++)
+            sb.append(base62chars[random.nextInt(62)]);
+        return sb.toString();
+    }
+
+    public void createId(){
+        String id;
+        do {
+            id = generateId();
+        }while(!db.exist(id));
     }
 
 
